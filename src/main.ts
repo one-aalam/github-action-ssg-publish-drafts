e05ee762677a5dc368149b16c4b79a8a7978f602
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-09-12 13:18:56 +0530

    first build (experimental)

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,39 @@
+
+import path from "path"
+const env = process.env;
+
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as glob from '@actions/glob'
+import { exec } from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const astroPath = core.getInput('astroPath') || '.'
+    core.debug(`Starting the scan for future posts...`)
+    const now = new Date();
+    let draftCount = 0;
+    const githubActor = core.getInput('github_actor', { required: true } )
+    const branch = core.getInput('branch')
+    const githubToken = core.getInput('github_token')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const gitUsername = core.getInput('git_username') || 'github-actions[bot]'
+    const gitEmail = core.getInput('git_email') || '41898282+github-actions[bot]@users.noreply.github.com'
+    const gitMessage = core.getInput('git_message') || 'Publish Drafts'
 
-    core.setOutput('time', new Date().toTimeString())
+    core.debug(`Started the scan at ${now}`)
+
+    const patterns = [path.join(astroPath, 'src', 'drafts', '*.md')]
+    const globber = await glob.create(patterns.join('\n'), { followSymbolicLinks: false })
+    for await (const file of globber.globGenerator()) {
+        draftCount += 1
+        console.log(file)
+    }
+
+    if (githubActor) {
+        await exec('git', ['config', '--global', 'user.email', gitEmail]);
+        await exec('git', ['config', '--global', 'user.name', gitUsername]);
+    }
+    core.setOutput('published', `Published ${draftCount} posts!`)
   } catch (error) {
     core.setFailed(error.message)
   }
